b31b484f64a2472bca7a58fe93e24c3b82ea8794
arthur <arthur.bourbousson@kitware.com>
2022-08-26 11:25:01 +0200

    [ROS][feat] Make lidar_slam package build and run in ROS2
    - Adapt packages dependencies for ROS2
    - Adapt publisher/subscriber with ROS2 template system
    - Adapt current time by linking it to the lidar_slam node time
    - Change some time types to be ROS2 compatible
    - Erase nodehandle system
    - Change print log system for ROS2
    - Adapt pointCloud for ROS2
    
    Note: Parameters are not functional

--- ros2_wrapping/lidar_slam/src/LidarSlamNode.h
+++ ros2_wrapping/lidar_slam/src/LidarSlamNode.h
@@ -20,6 +20,8 @@
 #define LIDAR_SLAM_NODE_H
 
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/publisher_base.hpp>
+#include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/static_transform_broadcaster.h>
@@ -34,21 +36,24 @@
 // SLAM
 #include <LidarSlam/Slam.h>
 
+#define PRINT_VERBOSE(minVerbosityLevel, stream) if (this->LidarSlam.GetVerbosity() >= (minVerbosityLevel)) {std::cout << stream << std::endl;}
 
-class LidarSlamNode
+#define PRINT_ERROR(s)   std::cerr << RED    << "[ERROR] "   << s << DEFAULT << std::endl;
+
+
+class LidarSlamNode : public rclcpp::Node
 {
 public:
 
   using PointS = LidarSlam::Slam::Point;
-  using CloudS = pcl::PointCloud<PointS>;  ///< Pointcloud needed by SLAM
+  using CloudS = pcl::PointCloud<PointS>;
 
   //----------------------------------------------------------------------------
   /*!
    * @brief     Constructor.
-   * @param[in] nh      Public ROS node handle, used to init publisher/subscribers.
-   * @param[in] priv_nh Private ROS node handle, used to access parameters.
+   * LidarSlamNode is directly the node used to init publisher/subscribers
    */
-  LidarSlamNode(rclcpp::Node::SharedPtr& nh, rclcpp::Node::SharedPtr& priv_nh);
+  LidarSlamNode(std::string name_node);
 
   //----------------------------------------------------------------------------
   /*!
@@ -58,6 +63,167 @@ public:
    */
   ~LidarSlamNode();
 
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     New main LiDAR frame callback, running SLAM and publishing TF.
+   * @param[in] cloud New frame, published by conversion node.
+   *
+   * Input pointcloud must have following fields :
+   *  - x, y, z (float): point coordinates
+   *  - time (double): time offset to add to the pointcloud header timestamp to
+   *    get approximate point-wise acquisition timestamp
+   *  - intensity (float): intensity/reflectivity of the point
+   *  - laser_id (uint16): numeric identifier of the laser ring that shot this point.
+   *    The lowest/bottom laser ring should be 0, and it should increase upward.
+   *  - device_id (uint8): numeric identifier of the LiDAR device/sensor.
+   *    This id should be the same for all points of the cloud acquired by the same sensor.
+   *  - label (uint8): optional input, not yet used.
+   */
+  virtual void ScanCallback(const sensor_msgs::msg::PointCloud2& pcl_msg);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     New secondary lidar frame callback, buffered to be latered processed by SLAM.
+   * @param[in] cloud New frame, published by conversion node.
+   *
+   * Input pointcloud must have following fields :
+   *  - x, y, z (float): point coordinates
+   *  - time (double): time offset to add to the pointcloud header timestamp to
+   *    get approximate point-wise acquisition timestamp
+   *  - intensity (float): intensity/reflectivity of the point
+   *  - laser_id (uint16): numeric identifier of the laser ring that shot this point.
+   *    The lowest/bottom laser ring should be 0, and it should increase upward.
+   *  - device_id (uint8): numeric identifier of the LiDAR device/sensor.
+   *    This id should be the same for all points of the cloud acquired by the same sensor.
+   *  - label (uint8): optional input, not yet used.
+   */
+  virtual void SecondaryScanCallback(const sensor_msgs::msg::PointCloud2& pcl_msg);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     Optional GPS odom callback, accumulating poses.
+   * @param[in] msg Converted GPS pose with its associated covariance.
+   */
+  void GpsCallback(const nav_msgs::msg::Odometry& msg);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     Optional tag detection callback, adding a landmark relative pose to the SLAM
+   * @param[in] msg april tag node output message
+   */
+  void TagCallback(const apriltag_ros::msg::AprilTagDetectionArray& tagInfo);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     Set SLAM pose from external guess.
+   * @param[in] msg The pose to use.
+   *
+   * NOTE: A valid TF tree must link msg.header.frame_id to OdometryFrameId.
+   * NOTE: The covariance is not used yet.
+   */
+  void SetPoseCallback(const geometry_msgs::msg::PoseWithCovarianceStamped& msg);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     Receive an external command to process, such as pose graph
+   *            optimization, GPS/SLAM calibration, set SLAM pose, save maps etc.
+   * @param[in] msg The command message.
+   */
+  void SlamCommandCallback(const lidar_slam_interfaces::msg::SlamCommand& msg);
+
+protected:
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief     Update transform offset between BASE and LIDAR using TF2
+   * @param[in] lidarFrameId The input LiDAR pointcloud frame_id.
+   * @param[in] lidarDeviceId The numerical identifier of the LiDAR sensor.
+   */
+  bool UpdateBaseToLidarOffset(const std::string& lidarFrameId, uint8_t lidarDeviceId);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief Publish SLAM outputs as requested by user.
+   *
+   * It is possible to send :
+   *  - pose and covariance as Odometry msg or TF
+   *  - extracted keypoints from current frame
+   *  - keypoints maps
+   *  - undistorted input points registered in odometry frame
+   */
+  void PublishOutput();
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief Get and fill Slam parameters from ROS parameters server.
+   */
+  void SetSlamParameters();
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief Fill the SLAM initial state with the given initial maps, pose and
+   *        landmarks.
+   */
+  void SetSlamInitialState();
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief Get and fill landmarks managers with absolute pose information
+   *        provided by the user in a csv file.
+   *        The fields must be : idx, x, y, z, roll, pitch, yaw, cov0, ..., cov35
+   *        The delimiters can be "," ";" " " "/t"
+   *        /!\ order matters
+   *        A header line can be added
+   */
+  void LoadLandmarks(const std::string& path);
+
+  //----------------------------------------------------------------------------
+  /*!
+   * @brief Build an id for the april tag output message
+   *        if it gives the info of one landmark, the id is the one of this landMark
+   *        if it gives the info of a tag bundle, the id is built as [idN [...] id1 id0]
+   */
+  int BuildId(const std::vector<int>& ids);
+
+  // Publish static tf to link world (UTM) frame to SLAM origin
+  // PGO must have been run, so we can average
+  // the correspondant poses (GPS/LidarSLAM) distances to get the offset
+  void BroadcastGpsOffset();
+
+  //----------------------------------------------------------------------------
+
+  // SLAM stuff
+  LidarSlam::Slam LidarSlam;
+  std::vector<CloudS::Ptr> Frames;
+
+  // ROS node handles, subscribers and publishers
+  std::vector<rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr> CloudSubs;
+  rclcpp::Subscription<lidar_slam_interfaces::msg::SlamCommand>::SharedPtr SlamCommandSub;
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr SetPoseSub;
+  std::unordered_map<int, rclcpp::PublisherBase::SharedPtr> Publishers;
+  std::unordered_map<int, bool> Publish;
+
+  // TF stuff
+  std::string OdometryFrameId = "odom";       ///< Frame in which SLAM odometry and maps are expressed.
+  std::string TrackingFrameId = "base_link";  ///< Frame to track (ensure a valid TF tree is published).
+  std::string GpsFrameId = "GPS"; ///< Frame to represent GPS positions.
+  rclcpp::Time GpsLastTime;
+  std::unique_ptr<tf2_ros::Buffer> TfBuffer;
+  std::shared_ptr<tf2_ros::TransformListener> TfListener;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> TfBroadcaster;
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> StaticTfBroadcaster;
+
+  // External sensors
+  // Landmarks
+  bool UseTags = false;
+  rclcpp::Subscription<apriltag_ros::msg::AprilTagDetectionArray>::SharedPtr LandmarkSub;
+  bool PublishTags = false;
+  LidarSlam::ExternalSensors::GpsMeasurement LastGpsMeas;
+
+  // GPS
+  bool UseGps = false; ///< Enable GPS data logging for Pose Graph Optimization or GPS/SLAM calibration.
+  Eigen::Isometry3d BaseToGpsOffset = Eigen::Isometry3d::Identity();  ///< Pose of the GPS antenna in BASE coordinates.
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr GpsOdomSub;
 };
 
 #endif // LIDAR_SLAM_NODE_H
--- ros2_wrapping/lidar_slam/src/LidarSlamNode_main.cxx
+++ ros2_wrapping/lidar_slam/src/LidarSlamNode_main.cxx
@@ -7,16 +7,13 @@
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::Node::SharedPtr nh = rclcpp::Node::make_shared("lidar_slam");
-  rclcpp::Node::SharedPtr priv_nh = rclcpp::Node::make_shared("~");
-
 
   // Create lidar slam node, which subscribes to pointclouds coming from conversion node
   // and to external sensor messages in parallel.
-  LidarSlamNode slam(nh, priv_nh);
+  std::shared_ptr<LidarSlamNode> slam = std::make_shared<LidarSlamNode>("lidar_slam");
 
   // Handle callbacks until shut down
-  rclcpp::spin(nh);
+  rclcpp::spin(slam);
 
   return 0;
 }
