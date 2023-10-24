60a2bc36a3b988dabece840d41939fdcc4710718
Jeanne Faure <jeanne.faure@kitware.com>
2023-10-24 17:58:20 +0200

    [feat] Create Vertex Map
    For now, the vertex map is a map of pointers to Point Features (PtFeat), whose index and depth only are initialized

--- slam_lib/src/DenseSpinningSensorKeypointExtractor.cxx
+++ slam_lib/src/DenseSpinningSensorKeypointExtractor.cxx
@@ -42,9 +42,94 @@ void DenseSpinningSensorKeypointExtractor::AddKeypoint(const Keypoint& k, const 
 }
 
 //-----------------------------------------------------------------------------
+std::shared_ptr<PtFeat> DenseSpinningSensorKeypointExtractor::GetPtFeat(int idxInScan) const
+{
+  auto& indices = this->Pc2VmIndices[idxInScan];
+  return this->VertexMap[indices.Row][indices.Col];
+}
+
+//-----------------------------------------------------------------------------
+int DenseSpinningSensorKeypointExtractor::GetScanLineSize(const std::vector<std::shared_ptr<PtFeat>>& scanLine)
+{
+  int nbPoints;
+  for (auto& point : scanLine)
+  {
+    if (point != nullptr)
+      nbPoints++;
+  }
+  return nbPoints;
+}
+
+//-----------------------------------------------------------------------------
+void DenseSpinningSensorKeypointExtractor::InitInternalParameters()
+{
+  // Map laser_ids
+  for (const auto& point : *this->Scan)
+  {
+    if (this->LaserIdMap.count(point.laser_id) == 0)
+      this->LaserIdMap[point.laser_id] = this->LaserIdMap.size();
+  }
+
+  // Save the number of lasers
+  this->NbLaserRings = this->LaserIdMap.size();
+
+  bool rotationIsClockwise = Utils::IsRotationClockwise<Point>(*this->Scan, this->NbLaserRings);
+
+  // Estimate azimuthal resolution if not already done
+  // or if the previous value found is not plausible
+  // (because last scan was badly formed, e.g. lack of points)
+  if (this->AzimuthalResolution < 1e-6 || M_PI/4. < this->AzimuthalResolution)
+    this->EstimateAzimuthalResolution();
+
+  // Compute the indices of scan points in the future vertex map
+  this->Pc2VmIndices.clear(); // Clear previous indices
+  this->Pc2VmIndices.reserve(this->Scan->size());
+  Eigen::Vector3f firstPt(this->Scan->at(0).data);
+  for (const auto& point : *this->Scan)
+  {
+    double cross = firstPt.x() * point.y - firstPt.y() * point.x;
+    double angle = std::atan2(cross, firstPt.head(2).dot(point.getVector3fMap().head(2)));
+    float azimuth = rotationIsClockwise ? M_PI + angle : M_PI - angle;
+    this->Pc2VmIndices.emplace_back(IdxVM{this->LaserIdMap.find(point.laser_id)->second,
+                                          static_cast<int>(azimuth / this->AzimuthalResolution)});
+  }
+}
+
+//-----------------------------------------------------------------------------
+void DenseSpinningSensorKeypointExtractor::CreateVertexMap()
+{
+  // Clear previous Vertex Map
+  for (auto& row : this->VertexMap)
+  {
+    for (auto& ptr : row)
+      ptr.reset(); // ptr = nullptr
+  }
+
+  this->WidthVM = 2.f * M_PI / this->AzimuthalResolution + 1;
+  this->HeightVM = this->NbLaserRings;
+
+  this->VertexMap.resize(this->HeightVM, std::vector<std::shared_ptr<PtFeat>>(this->WidthVM));
+  // Fill Vertex Map with index and depth values
+  #pragma omp parallel for num_threads(this->NbThreads) schedule(guided)
+  for (int i = 0; i < this->Scan->size(); ++i)
+  {
+    const Point& point = this->Scan->at(i);
+    auto& indices = this->Pc2VmIndices[i];
+
+    this->VertexMap[indices.Row][indices.Col] = std::make_shared<PtFeat>();
+    this->VertexMap[indices.Row][indices.Col]->Index = i;
+    this->VertexMap[indices.Row][indices.Col]->Depth = point.getVector3fMap().norm();
+  }
+}
+
+//-----------------------------------------------------------------------------
 void DenseSpinningSensorKeypointExtractor::ComputeKeyPoints(const PointCloud::Ptr& pc)
 {
-  //TODO
+  this->Scan = pc;
+
+  this->InitInternalParameters();
+
+  this->CreateVertexMap();
 }
 
 //-----------------------------------------------------------------------------
