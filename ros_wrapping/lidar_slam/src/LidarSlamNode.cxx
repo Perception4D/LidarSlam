7d0989f5a4ba355504c28e72712e5c75d0ef7820
Tong Fu <tong.fu@kitware.com>
2023-11-28 17:59:28 +0100

    [ROS][feat] Add clicked point callback
    This function is useful to get loop closure indices.
    In rviz, one can use Publish point to click on a pose and publish it
    as a 3D point on clicked_point topic. This 3D point refers to the pose
    that forms a loop with the current position.

--- ros_wrapping/lidar_slam/src/LidarSlamNode.cxx
+++ ros_wrapping/lidar_slam/src/LidarSlamNode.cxx
@@ -172,6 +172,9 @@ LidarSlamNode::LidarSlamNode(ros::NodeHandle& nh, ros::NodeHandle& priv_nh)
   if (this->UseExtSensor[LidarSlam::ExternalSensor::GPS])
     this->GpsOdomSub = nh.subscribe("gps_odom", 1, &LidarSlamNode::GpsCallback, this);
 
+  if (this->LidarSlam.IsPGOConstraintEnabled(LidarSlam::PGOConstraint::LOOP_CLOSURE))
+    this->ClickedPtSub = nh.subscribe("clicked_point", 1, &LidarSlamNode::ClickedPointCallback, this);
+
   // Init logging of landmark data, Camera data, external pose data, wheel encoder data and/or IMU data
   if (this->UseExtSensor[LidarSlam::ExternalSensor::LANDMARK_DETECTOR] ||
       this->UseExtSensor[LidarSlam::ExternalSensor::CAMERA] ||
@@ -693,6 +696,33 @@ void LidarSlamNode::ImuCallback(const sensor_msgs::Imu& imuMsg)
   else
     ROS_WARN_STREAM("The transform between the IMU and the tracking frame was not found -> IMU info ignored");
 
+}
+
+//------------------------------------------------------------------------------
+void LidarSlamNode::ClickedPointCallback(const geometry_msgs::PointStamped& pointMsg)
+{
+  if (!this->SlamEnabled)
+    return;
+
+  if (!this->LidarSlam.IsPGOConstraintEnabled(LidarSlam::PGOConstraint::LOOP_CLOSURE))
+    return;
+
+  // Get the clicked point in rviz
+  Eigen::Vector3d clickedPoint = Eigen::Vector3d(pointMsg.point.x, pointMsg.point.y, pointMsg.point.z);
+  double time = pointMsg.header.stamp.sec + pointMsg.header.stamp.nsec * 1e-9;
+  auto itState = this->LidarSlam.GetClosestState(clickedPoint);
+  if ((itState->Isometry.translation() - clickedPoint).squaredNorm() > 1.0)
+  {
+    ROS_WARN_STREAM("The clicked point is too far from the trajectory! Please pick another point.");
+    return;
+  }
+  // Add clicked point as revisited frame of the loop which is formed with current frame
+  LidarSlam::LoopClosure::LoopIndices loop(this->LidarSlam.GetLastState().Index, itState->Index, time);
+  this->LidarSlam.AddLoopClosureIndices(loop);
+  if (this->LidarSlam.GetVerbosity() >= 3)
+    ROS_INFO_STREAM("Loop closure point added with time "
+                    << std::fixed << std::setprecision(14)
+                    << time);
 }
 
 //------------------------------------------------------------------------------
