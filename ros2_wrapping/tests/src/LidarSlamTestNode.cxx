ad6678107b4c5edc00cfb5cbb29fc2cf59b5fbf6
julia.sanchez <julia.sanchez@kitware.com>
2023-12-21 15:49:02 +0100

    [tests][style] Minor changes
    * Long lines are cut
    * Upper/Lower cases are uniformized
    * Comments are enriched/corrected
    * Parameters are grouped
    * Spaces are added/removed

--- ros2_wrapping/tests/src/LidarSlamTestNode.cxx
+++ ros2_wrapping/tests/src/LidarSlamTestNode.cxx
@@ -121,11 +121,11 @@ double Normalize(double value)
 LidarSlamTestNode::LidarSlamTestNode(std::string name_node, const rclcpp::NodeOptions& options):
   Node(name_node, options)
 {
-  //  Compare or not the results with a reference
+  // Set the path to the reference if comparison is required
   this->get_parameter("ref_path", this->RefPath);
   if (!this->RefPath.empty())
   {
-    RCLCPP_INFO_STREAM(this->get_logger(), "Loading reference...");
+    RCLCPP_INFO_STREAM(this->get_logger(), "Loading reference at " << this->RefPath);
     this->LoadRef();
   }
   else
@@ -180,7 +180,7 @@ void LidarSlamTestNode::LoadRef()
   std::string line;
 
   // Fill the reference poses vector
-  while (getline(refPosesFile, line))
+  while (std::getline(refPosesFile, line))
   {
     // Temporal struct to store pose info
     Pose pose;
@@ -193,10 +193,10 @@ void LidarSlamTestNode::LoadRef()
     for (int i = 0; i < 5; ++i)
     {
       pos = line.find(" ");
-      pose.data(i) = std::stod(line.substr(0, pos));
+      pose.Data(i) = std::stod(line.substr(0, pos));
       line.erase(0, pos + 1);
     }
-    pose.data(5) = std::stod(line);
+    pose.Data(5) = std::stod(line);
     this->RefPoses.push_back(pose);
   }
   refPosesFile.close();
@@ -273,6 +273,7 @@ void LidarSlamTestNode::PoseCallback(const nav_msgs::msg::Odometry& poseMsg)
       ++this->PoseCounter;
   }
 
+  // No more reference
   if (this->PoseCounter == this->RefEvaluators.size())
   {
     this->OutputTestResult();
@@ -282,7 +283,7 @@ void LidarSlamTestNode::PoseCallback(const nav_msgs::msg::Odometry& poseMsg)
   // If the current frame has not been seen in reference -> return (wait for next frame)
   if (this->RefPoses[this->PoseCounter].Stamp - time > 1e-6)
   {
-    RCLCPP_WARN_STREAM(this->get_logger(), "Reference does not contain a frame at "
+    RCLCPP_WARN_STREAM(this->get_logger(), "Reference does not contain a pose at "
                      << std::fixed << std::setprecision(9) << time
                      << " (may have been dropped)."
                      << " Check the reference was computed on the same data.");
@@ -290,10 +291,10 @@ void LidarSlamTestNode::PoseCallback(const nav_msgs::msg::Odometry& poseMsg)
   }
 
   // Compare the pose with reference trajectory
-  Eigen::Isometry3d refTransform = Utils::XYZRPYtoIsometry(this->RefPoses[this->PoseCounter].data);
+  Eigen::Isometry3d refTransform = Utils::XYZRPYtoIsometry(this->RefPoses[this->PoseCounter].Data);
   Eigen::Isometry3d refPrevTransform;
   if (this->PoseCounter >= 1)
-    refPrevTransform = Utils::XYZRPYtoIsometry(this->RefPoses[this->PoseCounter - 1].data);
+    refPrevTransform = Utils::XYZRPYtoIsometry(this->RefPoses[this->PoseCounter - 1].Data);
   else
   {
     this->PrevTransform = transform;
@@ -309,7 +310,8 @@ void LidarSlamTestNode::PoseCallback(const nav_msgs::msg::Odometry& poseMsg)
   this->DiffPosition = Utils::Average(currentDiffPosition, this->DiffPosition, this->PoseCounter);
 
   // Test fails if any pose is too different from its reference pose
-  if (currentDiffPosition > this->PositionThreshold || currentDiffAngle * 180.f / M_PI > this->AngleThreshold)
+  if (currentDiffPosition > this->PositionThreshold ||
+      currentDiffAngle * 180.f / M_PI > this->AngleThreshold)
   {
     RCLCPP_ERROR_STREAM(this->get_logger(), "Pose at " << std::fixed << std::setprecision(9) << time << " is not consistent with reference");
     this->Failure = true;
@@ -376,7 +378,8 @@ void LidarSlamTestNode::ConfidenceCallback(const lidar_slam::msg::Confidence& co
       ++this->ConfidenceCounter;
   }
 
-  if (this->ConfidenceCounter >= this->RefEvaluators.size())
+  // No more reference
+  if (this->ConfidenceCounter == this->RefEvaluators.size())
   {
     this->OutputTestResult();
     return;
@@ -386,7 +389,7 @@ void LidarSlamTestNode::ConfidenceCallback(const lidar_slam::msg::Confidence& co
   // The last frame cannot be dropped so the node should be ended in any case.
   if (this->RefEvaluators[this->ConfidenceCounter].Stamp - time > 1e-6)
   {
-    RCLCPP_WARN_STREAM(this->get_logger(), "Reference does not contain a frame at "
+    RCLCPP_WARN_STREAM(this->get_logger(), "Reference does not contain an evaluator at "
                        << std::fixed << std::setprecision(9) << time
                        << " (may have been dropped)."
                        << " Check the reference was computed on the same data");
@@ -403,7 +406,8 @@ void LidarSlamTestNode::ConfidenceCallback(const lidar_slam::msg::Confidence& co
 
   if (this->Verbose)
   {
-    RCLCPP_INFO_STREAM(this->get_logger(), "Confidence difference for pose at " << std::fixed << std::setprecision(9) << time << ": \n"
+    RCLCPP_INFO_STREAM(this->get_logger(), "Confidence difference for pose at "
+                    << std::fixed << std::setprecision(9) << time << ": \n"
                     << "\t" << "Overlap difference: "            << 100 * diffOverlap << " %\n"
                     << "\t" << "Number of matches difference : " << diffNbMatches     << " matches\n"
                     << "\t" << "Computation time difference : "  << diffTime          << " s");
@@ -427,8 +431,9 @@ void LidarSlamTestNode::OutputTestResult()
     RCLCPP_ERROR_STREAM(this->get_logger(), "Computation time is too long compared to reference (" << this->DiffTime << "s longer)");
     this->Failure = true;
   }
+
   if (!this->Failure)
-    RCLCPP_INFO_STREAM(this->get_logger(), ("Test successfully passed"));
+    RCLCPP_INFO_STREAM(this->get_logger(), BOLD_GREEN("Test successfully passed"));
   else
     RCLCPP_ERROR_STREAM(this->get_logger(), "Test failed");
 
