6f557d8a2b320c5db620a44ab727e7bc1ea2b2f7
Tong Fu <tong.fu@kitware.com>
2024-01-11 14:55:50 +0100

    [debug][tmp] Remove debug change

--- ros_wrapping/lidar_conversions/src/VelodyneToLidarNode.cxx
+++ ros_wrapping/lidar_conversions/src/VelodyneToLidarNode.cxx
@@ -84,7 +84,7 @@ void VelodyneToLidarNode::Callback(const CloudV& cloudV)
   }
 
   if (this->RotationDuration < 0.)
-    this->RotationDuration = 0.1;
+    return;
 
   // Init SLAM pointcloud
   CloudS cloudS = Utils::InitCloudS<CloudV>(cloudV);
