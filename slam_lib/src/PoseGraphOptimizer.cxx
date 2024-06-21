b48b1304be54b01303db8c474c0ff5ca4523c326
Timothée Couble <timothee.couble@kitware.com>
2024-06-21 13:57:09 +0200

    [refact] Use std::make_unique for newer g2o version

--- slam_lib/src/PoseGraphOptimizer.cxx
+++ slam_lib/src/PoseGraphOptimizer.cxx
@@ -34,7 +34,7 @@ PoseGraphOptimizer::PoseGraphOptimizer()
 {
   // Create optimizer
   auto linearSolver = std::make_unique<g2o::LinearSolverEigen<g2o::BlockSolver_6_3::PoseMatrixType>>();
-  auto* solver = new g2o::OptimizationAlgorithmLevenberg(g2o::make_unique<g2o::BlockSolver_6_3>(std::move(linearSolver)));
+  auto* solver = new g2o::OptimizationAlgorithmLevenberg(std::make_unique<g2o::BlockSolver_6_3>(std::move(linearSolver)));
   this->Optimizer.setAlgorithm(solver);
   this->Optimizer.setVerbose(this->Verbose);
 }
