58f752750675a74326897dd7d3b205f86d2828ec
TongFu <tong.fu@kitware.com>
2025-03-31 14:05:31 +0200

    [fix] Fix frame id to publish tf of estimated calibration matrix

--- ros_wrapping/lidar_slam/src/LidarSlamNode.cxx
+++ ros_wrapping/lidar_slam/src/LidarSlamNode.cxx
@@ -1443,7 +1443,7 @@ void LidarSlamNode::SlamCommandCallback(const lidar_slam::SlamCommand& msg)
       //Publish new static TF
       geometry_msgs::TransformStamped tfStamped;
       tfStamped.header.stamp = ros::Time(ros::Time::now().toSec());
-      tfStamped.header.frame_id = this->OdometryFrameId;
+      tfStamped.header.frame_id = this->TrackingFrameId;
       tfStamped.child_frame_id = this->ExtPoseFrameId;
       tfStamped.transform = Utils::IsometryToTfMsg(calibration);
       this->StaticTfBroadcaster.sendTransform(tfStamped);
